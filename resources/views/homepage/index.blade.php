5be2774da2879341cce9f6b8a03631c6e3c12ddb
Lingkkkkkk <yuepengfei@kenrobot.com>
2018-02-10 00:31:28 +0800

    _index

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,10 +1,98 @@
 @extends('homepage.layout')
 
 @section('content')
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="assets/css/index.css">
 
-    <div>hello</div>
+    <div class="warp" style="min-height: 671px;">
+        <div class="banner">
+            <ul class="img">
+                <li class="active">
+                    <img src="assets/img/index/banner_1.jpg">
+                </li>
+                <li>
+                    <img src="assets/img/index/banner_2.jpg">
+                </li>
+            </ul>
+            <ul class="nav-img">
+                <li class="active"></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="inner clearfix">
+            <!-- 第一部分 -->
+            <div class="t1 clearfix">
+                <div class="title-img clearfix">
+                    <div class="img1">
+                        <img src="assets/img/index/title_1.png">
+                    </div>
+                    <div class="img2">
+                        <img src="assets/img/index/title_2.png">
+                    </div>
+                </div>
+                <div class="title-content clearfix">
+                    <div class="title">
+                        <span class="sp1">赛事动态</span>
+                        <span class="sp2">Dynamic Events</span>
+                        <a class="sp3">MORE></a>
+                    </div>
+                    <ul>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a><i>2018-02-08</i></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- 第二部分 -->
+            <div class="t2 clearfix">
+                <div class="title-content-left title-content-item clearfix">
+                    <div class="title">
+                        <span class="sp1">资源下载</span>
+                        <span class="sp2">Dynamic Events</span>
+                    </div>
+                    <ul>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                    </ul>
+                </div>
+                <div class="title-content-right title-content-item clearfix">
+                    <div class="title">
+                        <span class="sp1">赛事视频</span>
+                        <span class="sp2">Dynamic Events</span>
+                    </div>
+                    <ul>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                        <li><a href="#">关于2017年世界机器人大赛补充通知</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
+        $(function(){
+            $('.nav-img >li').each(function(index,el) {
+                $(this).click(function(){
+                    $('.nav-img >li').removeClass('active');
+                    $(this).addClass('active');
+
+                    $('.img >li').removeClass('active');
+                    $('.img li').eq(index).addClass('active');
+                })
+            })
+        })
     </script>
 @endsection
--- resources/views/homepage/layout.blade.php
+++ resources/views/homepage/layout.blade.php
@@ -7,13 +7,99 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="format-detection" content="telephone=no">
     <script src="../assets/js/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/layout.css">
 </head>
 <body>
-    <span>top</span>
+    <div class="top">
+        <div class="inner">
+            <div class="logo">
+                <img src="assets/img/common/logo.png">
+            </div>
+            <ul class="nav">
+                <li class="active">
+                    <a href="#">首页</a>
+                </li>
+                <li>
+                    <a href="#">赛事信息</a>
+                    <ul class="two-level">
+                        <li><a>赛事动态</a></li>
+                        <li><a>赛事介绍</a></li>
+                        <li><a>赛事相关</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">参赛报名</a>
+                    <ul class="two-level">
+                        <li><a>参赛报名</a></li>
+                        <li><a>公示公告</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">水平测试</a>
+                    <ul class="two-level" style="display: none;">
+                        <li><a>赛事动态</a></li>
+                        <li><a>赛事介绍</a></li>
+                        <li><a>赛事相关</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">资源下载</a>
+                </li>
+                <li>
+                    <a href="#">视频资源</a>
+                </li>
+                <li>
+                    <a href="#">关于我们</a>
+                    <ul class="two-level">
+                        <li><a>品牌介绍</a></li>
+                        <li><a>联系我们</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="user-dialog">
+                <div class="noLogin">
+                    <a id="register" href="#">注册</a><i>  /  </i><a id="login" href="#">登录</a>
+                </div>
+                <div class="login"></div>
+            </div>
+        </div>
+    </div>
     @yield('content')
-    <span>footer</span>
-
+    <div class="footer">
+        <div class="inner">
+            <div class="link">
+                <ul>
+                    <li><a href="index.html">网站首页</a></li>
+                    <li><a href="http://ss.robocom.com.cn" target="_blank">参赛报名</a></li>
+                    <li><a href="Event-information.html">赛事信息</a></li>
+                    <li><a href="Beauty-live.html">赛事直播</a></li>
+                    <li><a href="Prepare-for-registration.html">备战资源</a></li>
+                </ul>
+            </div>
+            <div id="reldescript" style="text-align: center;">
+                <span>Powered by RoboCom 国际公开赛组委会  © 2002-2020</span><br>
+                <span>技术支持：北京搜获科技有限公司</span><br>
+                <span>开发运营：通服(武汉)数字工程有限公司</span><br>
+                <span>CopyRight@北京搜获科技有限公司</span>
+            </div>
+            <div class="code">
+                <img src="assets/img/common/qrcode.png">
+            </div>
+        </div>
+    </div>
     <script>
+        $(function(){
+            var oNav = $('.nav >li');
+            oNav.each(function() {
+                $(this).hover(function() {
+                    oNav.removeClass('active')
+                    $(this).addClass('active')
+                }, function() {
+                    oNav.removeClass('active')
+                });
+            })
+        })
     </script>
 </body>
 </html>
