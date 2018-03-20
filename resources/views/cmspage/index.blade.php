e96f15b75d1ee62c0b173e33c86e4d47f45d74d0
Pengfei <663831709@qq.com>
2018-03-20 15:15:48 +0800

    修改cms首页内容 修改线上数据库cms_banner内容

--- resources/views/cmspage/index.blade.php
+++ resources/views/cmspage/index.blade.php
@@ -1,4 +1,5 @@
 <link href="/assets/css/cms_index.css" rel="stylesheet">
-<div>
-    <span class="tips">这里是首页提示信息</span>
+<div style="margin-top: 100px;">
+    <span class="tips" style="letter-spacing: 1px; line-height: 50px;display: block;text-align: center;">您好，欢迎来到RoboCom网站后台管理界面</span>
+    <span style="letter-spacing: 1px;line-height: 50px;display: block;text-align: center;font-size: 16px;">(请根据左侧导航栏进行相关操作)</span>
 </div>
